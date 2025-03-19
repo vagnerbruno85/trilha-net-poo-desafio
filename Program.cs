887e9c2460537554b429a63cf80fa2c8515dc16d
VagnerBatista85 <139138216+VagnerBatista85@users.noreply.github.com>
2025-03-19 10:11:51 -0300

    feat: Correção de models e Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456", "Nokia 3310", "111111", 16);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Notas");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("987654", "iPhone 14", "222222", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
